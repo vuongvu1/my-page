5dcff0431aaaa9a6bb7175587f85dc971f4e8879
vuongvu <vuong.vu@2359media.com>
2021-03-10 22:18:57 +0800

    Refactor DigitalClock

--- src/pages/playground/components/DigitalClock.tsx
+++ src/pages/playground/components/DigitalClock.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, memo } from "react";
-import { MONTHS_MAP } from "./utils";
+import { MONTHS_MAP, getCurrentTime } from "./utils";
 import SC from "./styles";
 
 const Block = memo(
@@ -22,27 +22,12 @@ const Block = memo(
   )
 );
 
-const extendNumber = (num: number): string => (num < 10 ? `0${num}` : `${num}`);
-
 const DigitalClock = () => {
-  const [num, setNum] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [time, setTime] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   useEffect(() => {
     const countInterval = setInterval(() => {
-      const currentDate = new Date();
-      const hours = currentDate.getHours();
-      const minutes = currentDate.getMinutes();
-      const seconds = currentDate.getSeconds();
-      const date = currentDate.getDate();
-      const month = currentDate.getMonth();
-
-      setNum(
-        `${extendNumber(hours)}${extendNumber(minutes)}${extendNumber(
-          seconds
-        )}${extendNumber(date)}${extendNumber(month)}`
-          .split("")
-          .map((e) => parseInt(e))
-      );
+      setTime(getCurrentTime());
     }, 1000);
 
     return () => clearInterval(countInterval);
@@ -53,19 +38,19 @@ const DigitalClock = () => {
       <SC.Title>Digital Clock</SC.Title>
       <SC.Clock>
         <SC.Seperator>Date:&nbsp;</SC.Seperator>
-        <Block total={4} current={num[6]} />
-        <Block total={10} current={num[7]} />
+        <Block total={4} current={time[6]} />
+        <Block total={10} current={time[7]} />
         <SC.Seperator>&nbsp;</SC.Seperator>
-        <Block total={12} current={parseInt(`${num[8]}${num[9]}`)} isMonth />
+        <Block total={12} current={parseInt(`${time[8]}${time[9]}`)} isMonth />
         <SC.Seperator>&nbsp;&nbsp;&nbsp;Time:&nbsp;</SC.Seperator>
-        <Block total={3} current={num[0]} />
-        <Block total={10} current={num[1]} />
+        <Block total={3} current={time[0]} />
+        <Block total={10} current={time[1]} />
         <SC.Seperator>&nbsp;</SC.Seperator>
-        <Block total={6} current={num[2]} />
-        <Block total={10} current={num[3]} />
+        <Block total={6} current={time[2]} />
+        <Block total={10} current={time[3]} />
         <SC.Seperator>&nbsp;</SC.Seperator>
-        <Block total={6} current={num[4]} />
-        <Block total={10} current={num[5]} />
+        <Block total={6} current={time[4]} />
+        <Block total={10} current={time[5]} />
       </SC.Clock>
     </SC.Wrapper>
   );
--- src/pages/playground/components/utils.ts
+++ src/pages/playground/components/utils.ts
@@ -12,3 +12,17 @@ export const MONTHS_MAP = [
   "Nov",
   "Dec",
 ];
+
+const extendNumber = (num: number): string => (num < 10 ? `0${num}` : `${num}`);
+
+export const getCurrentTime = (): Array<number> => {
+  const currentTime = new Date();
+  const hours = extendNumber(currentTime.getHours());
+  const minutes = extendNumber(currentTime.getMinutes());
+  const seconds = extendNumber(currentTime.getSeconds());
+  const date = extendNumber(currentTime.getDate());
+  const month = extendNumber(currentTime.getMonth());
+  const timeString = `${hours}${minutes}${seconds}${date}${month}`;
+
+  return timeString.split("").map((e: string) => parseInt(e));
+};
