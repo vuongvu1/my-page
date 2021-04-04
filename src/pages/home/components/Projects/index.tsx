83bee8bdd1f6371a39397cb619888d94488ca1b8
vuongvu <vuong.vu@2359media.com>
2021-04-05 01:31:56 +0800

    Add more projects

--- src/pages/home/components/Projects/index.tsx
+++ src/pages/home/components/Projects/index.tsx
@@ -1,8 +1,9 @@
 import { useTheme } from "styled-components";
-import { Section, Text, Card } from "atoms";
+import { Section, Text, Card, Link } from "atoms";
 import cat from "assets/images/cat.webp";
 import wiretap from "assets/images/wiretap.webp";
 import passgowhere from "assets/images/passgowhere.webp";
+import reactEasyLocalization from "assets/images/react-easy-localization.webp";
 import SC from "./styles";
 
 const Projects = () => {
@@ -23,16 +24,27 @@ const Projects = () => {
             A Chrome extension that lets users comment, share, socialize...
             while using Netflix.
           </Card>
-          <Card imgSrc={cat} title="Project Name">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          <Card
+            imgSrc={reactEasyLocalization}
+            title="react-easy-localization"
+            url="https://www.npmjs.com/package/react-easy-localization"
+          >
+            A simple Node module built on top of{" "}
+            <Link
+              href="https://www.npmjs.com/package/react-localization"
+              color={palette.primary.main}
+            >
+              react-localization
+            </Link>{" "}
+            package that helps React apps to implement language support easily.
           </Card>
-          <Card imgSrc={cat} title="Project Name">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            feugiat tortor velit, vitae commodo ex malesuada et.
+          <Card imgSrc={cat} title="Design Language System">
+            A private Node package that includes high-quality components to
+            create the same user experience for Singtel products.
           </Card>
           <Card
             imgSrc={passgowhere}
-            title="Pass Go Where"
+            title="PassGoWhere"
             url="https://pass.gowhere.gov.sg/"
           >
             A web app that allows Singapore foreign workers to apply for an Exit
