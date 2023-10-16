669c84a6647cf5a8451f0392fb690575436ffca8
Vuong Vu <hoangvuong.v99@gmail.com>
2023-10-16 18:27:03 +0200

    refactor: upgrade styled-components to the latest v6

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, StyleSheetManager } from "styled-components";
+import isPropValid from "@emotion/is-prop-valid";
 
 import "assets/fonts.css";
 import App from "./router";
@@ -10,13 +11,25 @@ import GlobalStyle, { theme } from "./globalStyles";
 const container = document.getElementById("root") as Element;
 const root = createRoot(container);
 
+// This implements the default behavior from styled-components v5
+function shouldForwardProp(propName: string, target: any) {
+  if (typeof target === "string") {
+    // For HTML elements, forward the prop if it is a valid HTML attribute
+    return isPropValid(propName);
+  }
+  // For other elements, forward all props
+  return true;
+}
+
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+  <StrictMode>
+    <StyleSheetManager shouldForwardProp={shouldForwardProp}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </StyleSheetManager>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
